c60b727dc611eae74b4bb1559eeb5d9d048dfe27
Jonas <jonas.tosti@telefonica.com>
2021-11-03 18:50:14 -0300

    Update Kubernetes Files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="jonastosti/udacity-project"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run kubernetes-project-udacity --image=$dockerpath
 
 
 # Step 3:
+# Wait for pods to be created
+kubectl wait --for=condition=ready pod kubernetes-project-udacity
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward kubernetes-project-udacity 8000:80
